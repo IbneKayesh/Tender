47d15d97e094b5e0a2724de6e6376f8936caada2
kazi Tareq Aziz <mis43@mis.prangroup.com>
2022-04-26 16:57:19 +0600

    bid request add

--- Tender.App/Controllers/TenderController.cs
+++ Tender.App/Controllers/TenderController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Tender.Models.Models;
+using Tender.App.Service;
 
 namespace Tender.App.Controllers
 {
@@ -18,12 +19,29 @@ namespace Tender.App.Controllers
         [HttpGet]
         public ActionResult BidRequest()
         {
+            DropDownFor_Tender();
             return View();
         }
         [HttpPost]
         public ActionResult BidRequest(RFQ_TENDER obj)
         {
+
+            DropDownFor_Tender();
             return View(obj);
         }
+
+        public void DropDownFor_Tender()
+        {
+            ViewBag.SELL_BUY = TenderService.DropDownList_Sel_Buy();
+            ViewBag.RE_BID = TenderService.getReBidding();
+            ViewBag.LOWER_RATE = TenderService.getAnyRate();
+            ViewBag.PRODUCTS_ID = TenderService.DropDownList_item();
+            ViewBag.PARTIAL_SHIPMENT = TenderService.DropDown_partialshipment();
+            ViewBag.SHIPMENT_MODE = TenderService.DropDown_shipmentMode();
+            ViewBag.PORT_ID = TenderService.DropDown_Port();
+            ViewBag.INCO_TERMS = TenderService.DropDown_Incoterms();
+
+        }
+
     }
 }
---
+++ Tender.App/Service/TenderService.cs
@@ -0,0 +1,89 @@
+﻿using Aio.Db.MSSqlEF;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Tender.Models.Models;
+
+namespace Tender.App.Service
+{
+    public class TenderService
+    {
+        public static SelectList DropDownList_Sel_Buy()
+        {
+            SelectList DataList = new SelectList(
+                  new List<SelectListItem>
+                  {new SelectListItem{ Text="Buy", Value = "0" },new SelectListItem{ Text="Sales", Value = "1" }}, "Value", "Text", 1);
+            return DataList;
+        }
+        public static List<SelectListItem> getAnyRate()
+        {
+            var objList = new List<SelectListItem>
+              {
+                new SelectListItem { Selected = true, Text = "Lower Rate Only", Value ="0"},
+                new SelectListItem { Selected = false, Text = "Any Rate", Value = "1"},
+              };
+            return objList;
+        }
+        public static List<SelectListItem> getReBidding()
+        {
+            var objList = new List<SelectListItem>
+              {
+                new SelectListItem { Selected = true, Text = "Submit Once", Value ="0"},
+                new SelectListItem { Selected = false, Text = "Submit Multiple", Value = "1"},
+            };
+            return objList;
+        }
+
+        public static SelectList DropDownList_item()
+        {
+            SelectList DataList = new SelectList(
+                  new List<SelectListItem>
+                  {new SelectListItem{ Text="CORN FLOWER", Value = "1" },
+                   new SelectListItem{ Text="WHEAT", Value = "2" },
+                   new SelectListItem{ Text= "PIG IRON-INDIA", Value = "3" },
+                   new SelectListItem{ Text="REFINED PALM OIL", Value = "4" }
+                  }, "Value", "Text", 1);
+            return DataList;
+        }
+        public static SelectList DropDown_partialshipment()
+        {
+            SelectList DataList = new SelectList(
+                  new List<SelectListItem>
+                  {new SelectListItem{ Text="Allow", Value = "1" },
+                   new SelectListItem{ Text="Not Allow", Value = "0" }
+                  }, "Value", "Text", 1);
+            return DataList;
+        }
+        public static SelectList DropDown_shipmentMode()
+        {
+            SelectList DataList = new SelectList(
+                  new List<SelectListItem>
+                  {new SelectListItem{ Text="Sea", Value = "1" },
+                   new SelectListItem{ Text="Road", Value = "2" },
+                   new SelectListItem{ Text="Air", Value = "3" }
+                  }, "Value", "Text", 1);
+            return DataList;
+        }
+        public static SelectList DropDown_Port()
+        {
+            SelectList DataList = new SelectList(
+                  new List<SelectListItem>
+                  {new SelectListItem{ Text="abc", Value = "1" },
+                   new SelectListItem{ Text="asdfasdf", Value = "2" },
+                   new SelectListItem{ Text="asdfasd", Value = "3" }
+                  }, "Value", "Text", 1);
+            return DataList;
+        }
+        public static SelectList DropDown_Incoterms()
+        {
+            SelectList DataList = new SelectList(
+                  new List<SelectListItem>
+                  {new SelectListItem{ Text="cfr", Value = "1" },
+                   new SelectListItem{ Text="cob", Value = "2" },
+                   new SelectListItem{ Text="cpt", Value = "3" }
+                  }, "Value", "Text", 1);
+            return DataList;
+        }
+
+    }
+}
