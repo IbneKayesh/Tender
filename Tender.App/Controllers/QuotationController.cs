ff0088e45c50d5377f983097a33aa9f389e6c8c8
kazi Tareq Aziz <mis43@mis.prangroup.com>
2022-04-28 09:03:38 +0600

    desing card

--- Tender.App/Controllers/QuotationController.cs
+++ Tender.App/Controllers/QuotationController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Tender.App.Service;
 using Tender.Models.Models;
 
 namespace Tender.App.Controllers
@@ -12,16 +13,32 @@ namespace Tender.App.Controllers
         // GET: Bid, For Bidder
         public ActionResult Index()
         {
+            DropDownFor_Tender();
             return View();
         }
         
+        [HttpGet]
         public ActionResult Bid(string id)
         {
+            DropDownFor_Tender();
             return View(new RFQ_BIDDING());
         }
+        [HttpPost]
         public ActionResult Bid(RFQ_BIDDING obj)
         {
             return View();
+        }
+        public void DropDownFor_Tender()
+        {
+            ViewBag.SELL_BUY = TenderService.DropDownList_Sel_Buy();
+            ViewBag.RE_BID = TenderService.getReBidding();
+            ViewBag.LOWER_RATE = TenderService.getAnyRate();
+            ViewBag.PRODUCTS_ID = TenderService.DropDownList_item();
+            ViewBag.PARTIAL_SHIPMENT = TenderService.DropDown_partialshipment();
+            ViewBag.SHIPMENT_MODE = TenderService.DropDown_shipmentMode();
+            ViewBag.PORT_ID = TenderService.DropDown_Port();
+            ViewBag.INCO_TERMS = TenderService.DropDown_Incoterms();
+
         }
     }
 }
